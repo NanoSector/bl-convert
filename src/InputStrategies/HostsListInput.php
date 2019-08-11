f925fd9672dd97d4f67486550209ea62f27d3627
NanoSector <rick@nanosector.nl>
2019-08-11 20:57:04 +0200

    Add hosts-based input support

---
+++ src/InputStrategies/HostsListInput.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Yoshi2889\BlConvert\InputStrategies;
+
+use Yoshi2889\BlConvert\Exceptions\InvalidFileFormatException;
+use Yoshi2889\BlConvert\Exceptions\UnreadableFileException;
+use Yoshi2889\BlConvert\ListTypes\HostnameList;
+
+class HostsListInput implements ListInputStrategy
+{
+    /**
+     * @inheritDoc
+     */
+    public static function fromFile(string $filename): HostnameList
+    {
+        $resource = @fopen($filename, 'rb');
+        if (!$resource) {
+            throw new UnreadableFileException('Unable to read the given file: ' . $filename);
+        }
+
+        $hostnames = [];
+        while (($line = fgets($resource)) !== false) {
+            $line = trim($line);
+
+            if (empty($line) || strpos($line, '#') === 0) {
+                continue;
+            }
+
+            $pieces = explode(' ', $line);
+
+            if (count($pieces) < 2) {
+                throw new InvalidFileFormatException('The given hosts-based list contains an invalid rule: ' . $line);
+            }
+
+            // the first element is not a domain we should block
+            array_shift($pieces);
+
+            // this is less resource intensive than array_merge
+            foreach ($pieces as $piece) {
+                $hostnames[] = $piece;
+            }
+        }
+
+        return new HostnameList($hostnames);
+    }
+}
---
+++ tests/HostsListInputTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Yoshi2889\BlConvert\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yoshi2889\BlConvert\Exceptions\InvalidFileFormatException;
+use Yoshi2889\BlConvert\Exceptions\UnreadableFileException;
+use Yoshi2889\BlConvert\InputStrategies\HostsListInput;
+
+class HostsListInputTest extends TestCase
+{
+
+    public function testFromFile()
+    {
+        $expected = [
+            'test.com',
+            'testing.com',
+            'foo.bar',
+            'bar.baz'
+        ];
+
+        $output = HostsListInput::fromFile(__DIR__ . '/testInputFiles/hostsinput.txt');
+        $this->assertEquals($expected, $output->getHostnames());
+
+        $outputComments = HostsListInput::fromFile(__DIR__ . '/testInputFiles/hostsinput_comments.txt');
+        $this->assertEquals($expected, $output->getHostnames());
+    }
+
+    public function testFromInvalidFile()
+    {
+        $this->expectException(InvalidFileFormatException::class);
+        HostsListInput::fromFile(__DIR__ . '/testInputFiles/hostsinput_invalid.txt');
+    }
+
+    public function testFromMissingFile()
+    {
+        $this->expectException(UnreadableFileException::class);
+        HostsListInput::fromFile(__DIR__ . '/testInputFiles/hostsinput_missing.txt');
+    }
+}
